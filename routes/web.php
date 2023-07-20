40081decaf24bc5542eba366d1d664bd4986154b
jumper01 <52915147+jumper01@users.noreply.github.com>
2023-07-20 10:16:00 +0100

    fix

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ use App\Http\Controllers\Test1Controller;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('overpassApi/zip-codes/{zipCode}/{radius}', [Test1Controller::class, 'getSurroundingZipCodesOverpassApi'])->name('overpass_get_zipCodes');
 // not needed anymore
-// Route::get('overpassApi/zip-codes/{zipCode}/{radius}', [Test1Controller::class, 'getSurroundingZipCodesOverpassApi'])->name('overpass_get_zipCodes');
 // Route::post('excel/fill-zip-codes', [Test1Controller::class, 'fillZipCodesFromExcel'])->name('fill_zipCodes_from_excel');
